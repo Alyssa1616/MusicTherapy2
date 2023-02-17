04e2752641d57fab211697af11ffdd2e35a38b3a
Alyssa Feinberg <alyssasf16@gmail.com>
2023-02-17 10:41:56 -0500

    Initial Commit

---
+++ MusicTherapy2/MusicTherapy2App.swift
@@ -0,0 +1,17 @@
+//
+//  MusicTherapy2App.swift
+//  MusicTherapy2
+//
+//  Created by Alyssa Feinberg on 2/17/23.
+//
+
+import SwiftUI
+
+@main
+struct MusicTherapy2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
